95668a28acb1bd58a84ef58d886899d7b890842c
Adrian Gomez <adrian.gomez@moove-it.com>
2015-06-30 12:35:58 -0300

    Allow primitive types in responses

--- lib/swaggard/swagger/response.rb
+++ lib/swaggard/swagger/response.rb
@@ -2,6 +2,8 @@ module Swaggard
   module Swagger
     class Response
 
+      PRIMITIVE_TYPES = %w[number string integer float boolean]
+
       attr_reader :status_code
 
       def initialize(status_code, value)
@@ -30,15 +32,20 @@ module Swaggard
       def response_model
         if @is_array_response
           {
-            'type'           => 'array',
-            'items'          => {
-              '$ref'        => "#/definitions/#@response_class"
-            }
+            'type'  => 'array',
+            'items' => response_class_type
           }
+        else
+          response_class_type
+        end
+      end
+
+      def response_class_type
+        if PRIMITIVE_TYPES.include?(@response_class)
+          { 'type' => @response_class }
         else
           { '$ref' => "#/definitions/#@response_class" }
         end
-
       end
 
     end
