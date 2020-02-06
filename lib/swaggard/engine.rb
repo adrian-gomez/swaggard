4484b72ebf9e1b8e6bb4c572d37100a2aaad310b
Adrian Gomez <adri4n.steam@gmail.com>
2020-02-06 12:47:07 -0300

    Only show sprockets warning on dev's console and server

--- lib/swaggard/engine.rb
+++ lib/swaggard/engine.rb
@@ -1,18 +1,22 @@
-unless Rails::Application.instance_methods.include?(:assets_manifest)
-  warn <<~END
-    [Swaggard] It seems you are using an api only rails setup, but swaggard
-    [Swaggard] web app needs sprockets in order to work. Make sure to add
-    [Swaggard] require 'sprockets/railtie'.
-    [Swaggard] If you plan to use it
-  END
-end
-
 module Swaggard
   class Engine < ::Rails::Engine
     isolate_namespace Swaggard
 
+    def rake?
+      File.basename($PROGRAM_NAME) == 'rake'
+    end
+
     initializer 'swaggard.finisher_hook', after: :finisher_hook do |app|
       app.reload_routes!
+
+      if Rails.env.development? && !rake? && !app.methods.include?(:assets_manifest)
+        warn <<~END
+        [Swaggard] It seems you are using an api only rails setup, but swaggard
+        [Swaggard] web app needs sprockets in order to work. Make sure to add
+        [Swaggard] require 'sprockets/railtie'.
+        [Swaggard] If you plan to use it
+        END
+      end
 
       Swaggard.configure do |config|
         unless config.controllers_path
